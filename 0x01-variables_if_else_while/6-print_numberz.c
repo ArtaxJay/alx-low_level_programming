a9e1786683cc3223f5dbadf3990f954e77e51d03
ArtaxJay <okjaceauthor@gmail.com>
2023-06-16 20:31:51 -0700

    Add 6-*

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Lorem ipsum
+ * Return: dolor
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
