5fa95501430e1c500ea98c4072b63ed37b66aa3b
ArtaxJay <okjaceauthor@gmail.com>
2023-06-16 20:05:14 -0700

    Add 3-*

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Lorem ipsum
+ * Return: Dolor
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}
