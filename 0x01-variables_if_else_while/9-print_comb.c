78a5eb8b4934294762ec6480df84eaf74597f043
ArtaxJay <okjaceauthor@gmail.com>
2023-06-16 20:53:30 -0700

    Add 9*

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - lorem ipsum
+ * Return: dolor
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
