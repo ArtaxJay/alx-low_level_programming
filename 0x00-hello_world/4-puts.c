51b88fef93b9e2d65f52af817ce220f0491a71e2
ArtaxJay <okjaceauthor@gmail.com>
2023-06-18 13:16:22 -0700

    Add some file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Lorem ipsum
+ * Return: dolor
+ */
+
+int main(void)
+{
+	puts("\"Programmming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Lorem ipsum
+ * Return: Dolor
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
