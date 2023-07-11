4caef27d481af7e70f494892de57a67f52a4d21e
ArtaxJay <okjaceauthor@gmail.com>
2023-07-11 09:35:43 +0000

    Modify the files in this dir

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 #include <stdlib.h>
@@ -18,13 +17,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 
-        str = malloc(sizeof(char) * size);
-
-        if (size == 0 || str == NULL)
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
-
 	for (i = 0; i < size; i++)
 		str[i] = c;
-
-        return (str);
+	return (str);
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,48 +5,31 @@
 #include "main.h"
 
 /**
-
  * _strdup - duplicate to new memory space location
-
  * @str: char
-
  * Return: 0
-
  */
 
 char *_strdup(char *str)
-
 {
+	char *aaa;
+	int i, r = 0;
 
-        char *aaa;
+	if (str == NULL)
+		return (NULL);
 
-        int i, r = 0;
+	i = 0;
 
+	while (str[i] != '\0')
+		i++;
 
-        if (str == NULL)
+	aaa = malloc(sizeof(char) * (i + 1));
 
-                return (NULL);
+	if (aaa == NULL)
+		return (NULL);
 
-        i = 0;
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
 
-        while (str[i] != '\0')
-
-                i++;
-
-
-        aaa = malloc(sizeof(char) * (i + 1));
-
-
-        if (aaa == NULL)
-
-                return (NULL);
-
-
-        for (r = 0; str[r]; r++)
-
-                aaa[r] = str[r];
-
-
-        return (aaa);
-
+	return (aaa);
 }
