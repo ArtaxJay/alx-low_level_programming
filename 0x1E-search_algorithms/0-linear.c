78e36191a832583015600ec4a648c46759df1216
ArtaxJay <okjaceauthor@gmail.com>
2024-02-06 15:48:10 +0000

    Refactor 0-*

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,7 +6,7 @@
  * @size: this args is the number of elements in the array
  * @value: this arg is the target to lookup in the array
  * Return: -1 if value is not found
- * 	In match: ret the first occurrence
+ *	In match: ret the first occurrence
  */
 
 int linear_search(int *array, size_t size, int value)
