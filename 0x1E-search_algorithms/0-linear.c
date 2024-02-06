00b7c1a864b75181aa5dd15ded9ceaea72cc2380
ArtaxJay <okjaceauthor@gmail.com>
2024-02-06 15:42:37 +0000

    Add a readme file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - do a linear search on an array
+ * @array: ptr to the first array member to search
+ * @size: this args is the number of elements in the array
+ * @value: this arg is the target to lookup in the array
+ * Return: -1 if value is not found
+ * 	In match: ret the first occurrence
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int j;
+
+	if (array == NULL)
+		return (-1);
+
+	for (j = 0; j < (int)size; j++)
+	{
+		printf("Value checked array[%u] = [%d]\n", j, array[j]);
+		if (array[j] == value)
+			return (j);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,14 @@
+# This is an ALX SE Project
+## 0x1E. C - Search Algorithms
+- C
+- Algorithm
+
+## The General Learning Objectives
+1. What is a search algorithm
+2. What is a linear search
+3. What is a binary search
+4. What is the best search algorithm to use depending on your needs
+
+
+**_This Project is done by_**
+# ArtaxJay Olaoluwa
